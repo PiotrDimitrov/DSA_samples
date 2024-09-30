8cad7114fe30286cec2faf4fe9fcdd57b459994f
PiotrDimitrov <piotr.dimitrov2019@gmail.com>
2024-09-30 17:09:21 +0300

    AVL tree begin

--- Data_Structures/myAVLtree.h
+++ Data_Structures/myAVLtree.h
@@ -4,7 +4,15 @@
 #include <vector>
 template <typename T>
 class myAVLtree {
-
+    class Node {
+        Node* right;
+        Node* left;
+        Node* prev;
+        T data;
+        Node() : left(nullptr), right(nullptr), prev(nullptr) {};
+        Node(T value) : left(nullptr), right(nullptr), prev(nullptr),  data(value) {};
+        friend class myAVLtree;
+    };
 };
 
 #endif //DSA_SAMPLES_MYAVLTREE_H
